c7d4a57ada3566c0671e9c1caf7900f1f31d4ef8
Artemii Malyshev <115695294+artemiithefrog@users.noreply.github.com>
2024-10-01 18:51:51 +0200

    Initial Commit

---
+++ BackgroundPhotoTest/BackgroundPhotoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BackgroundPhotoTestApp.swift
+//  BackgroundPhotoTest
+//
+//  Created by artemiithefrog . on 01.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct BackgroundPhotoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
